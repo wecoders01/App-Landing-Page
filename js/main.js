70acc6317a8851a663cec6064f27ea9c577c2478
Tanvir Ahamed <thisistanvir143@gmail.com>
2021-01-03 14:46:33 +0600

    Design Features Area with OwlCarousel

--- js/main.js
+++ js/main.js
@@ -27,6 +27,26 @@ $('.video-play-btn, .video-popup').on('click', function(){
 })
 
 
+/* ---------- Features Carousel ------------- */
+$('.feature-carousel').owlCarousel({
+   loop:true,
+   margin:0,
+   autoplay: true,
+   responsiveClass:true,
+   responsive:{
+       0:{
+           items:1,
+       },
+       600:{
+           items:2,
+       },
+       1000:{
+           items:3,
+       }
+   }
+})
+
+
 
 
 })
